f424b551fa247202e786d31c84cf67479014ee76
chandan <kumarchandan41u@gmail.com>
2022-01-10 20:58:53 +0530

    fluter wediget

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,32 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-       
+      home: Center(
+        child: RichText(
+          text: TextSpan(
+            text: 'fist',
+            style: TextStyle(fontSize: 20, color: Colors.deepPurple),
+            children: <TextSpan>[
+              TextSpan(
+                text: '2nd',
+                style: TextStyle(fontSize: 25, color: Colors.deepPurple),
+              ),
+              TextSpan(
+                text: ' 3nd',
+                style: TextStyle(
+                    fontSize: 30, color: Color.fromARGB(255, 206, 203, 34)),
+              ),
+              TextSpan(
+                text: '4rd',
+                style: TextStyle(fontSize: 40, color: Colors.deepPurple),
+              ),
+              TextSpan(
+                text: 'hello duniya',
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
